acab063c17bd4e7fa8ad2eff909c6db4aca895f9
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-04-23 08:32:24 -0400

    Updated from Brython Server: 4/23/2018 8:32:26 AM

--- middleWord.py
+++ middleWord.py
@@ -7,3 +7,5 @@ words = input('Enter a list of words: ').split(' ')
 if len(words)%2 ==0:
     print(words[len(words)/2])
     print(words[(len(words)/2)-1])
+else:
+    print(words[(len(words)/2)-.5])
