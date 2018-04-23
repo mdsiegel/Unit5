603e6630511862043b08ff1696d9bd86e710d954
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-04-23 08:21:38 -0400

    Updated from Brython Server: 4/23/2018 8:21:40 AM

--- fwordDemo.py
+++ fwordDemo.py
@@ -1,1 +1,9 @@
+#Matthew Siegel
+#4/23/18
+#fwordDemo.py - Finding words that start with F
 
+words = input('Type in some words: ').split(' ')
+
+for item in words:
+    if 'f' in item or 'F' in item:
+        print(item)
