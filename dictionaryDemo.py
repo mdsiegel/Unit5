002a1d78ac4704cbdea2f8916066d48a3ce8f6e6
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-04-25 08:07:20 -0400

    Updated from Brython Server: 4/25/2018 8:07:21 AM

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -1,1 +1,9 @@
+#Matthew Siegel
+#4/25/18
+#dictionaryDemo.py-more list practice
 
+words = ['computer','mortify','dog','firetruck','yes','python','cat']
+words.sort()
+
+num = int(input('What number word do you want? '))
+print(words[num-1])
