80c64cec78c56e27bf1bc5e8c545715e202e8370
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-04-25 08:09:30 -0400

    Updated from Brython Server: 4/25/2018 8:09:31 AM

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -6,4 +6,7 @@ words = ['computer','mortify','dog','firetruck','yes','python','cat']
 words.sort()
 
 num = int(input('What number word do you want? '))
-print(words[num-1])
+if num<=0 or num>=8:
+    print('error')
+else:
+    print(words[num-1])
