8935ba4b5d3e4ee6c57ce456af7d5971571f44c9
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-04-30 08:37:00 -0400

    Updated from Brython Server: 4/30/2018 8:37:00 AM

--- toDoList.py
+++ toDoList.py
@@ -1,1 +1,13 @@
+#Matthew Siegel
+#4/30/18
+#toDoList.py - Making a to do list
 
+print('Functions are add, delete, and print')
+
+Quit = False
+L = []
+while Quit == False:
+    text = input('>')
+    if text[0] == add:
+        L.append(text[1:len(text)])
+   
