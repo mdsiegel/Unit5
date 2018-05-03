524defd08b39b20972f79fe30cbcf13eb847926d
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-03 08:27:35 -0400

    Updated from Brython Server: 5/3/2018 8:27:34 AM

--- mySort.py
+++ mySort.py
@@ -1,1 +1,52 @@
+#Matthew Siegel
+#5/3/18
+#mySort.py - making a sort
 
+from random import randint
+from time import time
+
+N = 10 #how many numbers will be sorted
+
+def mySort(A):
+    if len(A)<= 1:
+        return A
+    left = []
+    right = []
+    for i in range(0,len(A)):
+        if i<len(A)/2:
+            left.append(A[i])
+        else:
+            right.append(A[i])
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return merge(left,right)
+
+def merge(left, right):
+    result = []
+    while len(left) > 0 and len(right) > 0:
+        if left[0] <= right[0]:
+            result.append(left[0])
+            left.remove(left[0])
+        else:
+            result.append(right
+
+if __name__ == '__main__':
+
+    #make a list of N random numbers between 1 and N
+    numbers = [0]*N
+    for i in range(N):
+        numbers[i] = randint(1,N)
+    
+    pythonSort = sorted(numbers) #Python's sort
+    
+    #time how long your sort takes
+    t1 = time()
+    numbers = mySort(numbers)
+    t2 = time()
+    
+    #print whether the sort worked or not
+    try:
+        assert(numbers == pythonSort)
+        print('Your sort took', t2-t1, 'seconds')
+    except:
+        print('Your sort did not work')
