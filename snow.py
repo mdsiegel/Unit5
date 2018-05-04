20f1bfc0f1ed926f9ca60f06d4edff22d7529011
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-04 08:39:26 -0400

    Updated from Brython Server: 5/4/2018 8:39:25 AM

--- snow.py
+++ snow.py
@@ -12,12 +12,12 @@ HEIGHT = 600
 def step():
     moreSnow()
     for snow in data['snowList']:
-        snow.x += randint(-1,3)
-        snow.y += randint(-1,3)
+        snow.x += randint(-1,1)
+        snow.y += randint(1,5)
 def moreSnow():
     data['frames'] +=1
     if data['frames']%50 == 0:
-        data['snowList'].append(Sprite(snow,(randint(1,WIDTH),randint(1,HEIGHT))))
+        data['snowList'].append(Sprite(snow,(randint(1,WIDTH),0)))
 
 
 #Putting rire ants randomly on the screen
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     data['snowList'] = []
     data['frames'] = 0
     white = Color(0xFFFFFF,1)
-    snow = CircleAsset(50,LineStyle(1,black),white)
+    snow = CircleAsset(10,LineStyle(1,black),white)
     
     black = Color(0x000000,1)
     line = LineStyle(3,black)
