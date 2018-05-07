812f13e6fb4580880f2895585f272f34d283b3fc
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2018-05-07 08:14:01 -0400

    Updated from Brython Server: 5/7/2018 8:14:00 AM

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,17 @@
+#Matthew Siegel
+#5/7/18
+#quiz5.py - list quiz
+
+def penultimate(A):
+    return A[-1]
+    
+def plusEquals(B,C):
+    for b in B:
+        b +=C
+    return B
+
+print(penultimate([3,4,5,6,7]))
+print(plusEquals([1,2,3,4],10))
+
+
 
